768df24afa4b35d57a45cd717b4d9a17e479cc9d
kenee101 <elijah.usih@stu.cu.edu.ng>
2025-11-11 12:53:40 +0100

    fixed variables

--- src/config/enviroment.validation.ts
+++ src/config/enviroment.validation.ts
@@ -4,11 +4,11 @@ export default Joi.object({
   NODE_ENV: Joi.string()
     .valid('development', 'production', 'test', 'provision')
     .default('development'),
-  DATABASE_PORT: Joi.number().port().default(5432),
-  DATABASE_PASSWORD: Joi.string().required(),
-  DATABASE_HOST: Joi.string().required(),
-  DATABASE_NAME: Joi.string().required(),
-  DATABASE_USER: Joi.string().required(),
+  DB_PORT: Joi.number().port().default(5432),
+  DB_PASSWORD: Joi.string().required(),
+  DB_HOST: Joi.string().required(),
+  DB_DATABASE: Joi.string().required(),
+  DB_USERNAME: Joi.string().required(),
   // PROFILE_API_KEY: Joi.string().required(),
   JWT_SECRET: Joi.string().required(),
   JWT_TOKEN_AUDIENCE: Joi.required(),
